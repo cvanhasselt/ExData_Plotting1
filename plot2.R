b812ae4907e24dd9720e5d8e1a5b2566a313bf1a
cvanhasselt <cvanhasselt@mindspring.com>
2014-07-12 19:33:53 -0400

    First 3 plots

---
+++ plot2.R
@@ -0,0 +1,32 @@
+# Read in data from the supplied dataset
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+# Convert the Date and Time columns to a new column, named DateTime
+hpc$DateTime <- strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+
+# subset the date, finding the date from two days in February of 2007
+hpcSubset <- hpc[ !is.na(hpc$DateTime) & as.Date(hpc$DateTime) >= "2007-02-01" & as.Date(hpc$DateTime) <= "2007-02-02" ,]
+
+# Replace "?" with NA value
+hpcSubset[hpcSubset$Global_active_power=="?"] <- NA
+hpcSubset[hpcSubset$Global_reactive_power=="?"] <- NA
+hpcSubset[hpcSubset$Voltage=="?"] <- NA
+hpcSubset[hpcSubset$Global_intensity=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_1=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_2=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_3=="?"] <- NA
+
+# Open PNG graphics device
+png("plot2.png",width=480,height=480)
+
+# create the plot 
+with(hpcSubset, 
+     plot(DateTime, 
+          as.numeric( Global_active_power)/500,
+          type="o",
+          pch=NA,
+          ylab="Global Active Power (Kilowatts)",
+          xlab=""))
+
+# close graphics devices
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,51 @@
+# Read in data from the supplied dataset
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+
+# Convert the Date and Time columns to a new column, named DateTime
+hpc$DateTime <- strptime(paste(hpc$Date,hpc$Time),"%d/%m/%Y %H:%M:%S")
+
+# subset the date, finding the date from two days in February of 2007
+hpcSubset <- hpc[ !is.na(hpc$DateTime) & as.Date(hpc$DateTime) >= "2007-02-01" & as.Date(hpc$DateTime) <= "2007-02-02" ,]
+
+# Replace "?" with NA value
+hpcSubset[hpcSubset$Global_active_power=="?"] <- NA
+hpcSubset[hpcSubset$Global_reactive_power=="?"] <- NA
+hpcSubset[hpcSubset$Voltage=="?"] <- NA
+hpcSubset[hpcSubset$Global_intensity=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_1=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_2=="?"] <- NA
+hpcSubset[hpcSubset$Sub_metering_3=="?"] <- NA
+
+# Open PNG graphics device
+png("plot1.png",width=480,height=480)
+
+# column values are factors, so this should be accounted for when graphing
+# Division by 500 scales of values into KW.
+hist(as.numeric(hpcSubset$Global_active_power)/500,
+     col="red", 
+     xlab="Global Active Power (Kilowatts)",
+     main="Global Active Power")
+
+# dev.off()
+png("plot3.png",width=480,height=480)
+plot(hpcSubset$DateTime, 
+    as.numeric(as.character(hpcSubset$Sub_metering_1)),
+    type="l",
+    ylab="Energy Sub-Metering",
+    xlab="",
+    yaxt="n",
+    col="black")
+axis(2,at=c(0,10,20,30))
+lines(hpcSubset$DateTime,
+    as.numeric(as.character(hpcSubset$Sub_metering_2)),
+    type="l",
+    col="red",
+    lwd=1)
+lines(hpcSubset$DateTime,
+    as.numeric(as.character(hpcSubset$Sub_metering_3)),
+    type="l",
+    col="blue",
+    lwd=1)
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1)
+
+dev.off()
